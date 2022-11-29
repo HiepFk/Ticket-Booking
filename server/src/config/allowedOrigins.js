5e85287d5426949b1c7579904053497f848adac9
HiepFK <hiepnh.fk@gmail.com>
2022-11-29 22:13:53 +0700

    update

--- server/src/config/allowedOrigins.js
+++ server/src/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
   process.env.CLIENT_URL,
   process.env.ADMIN_URLL,
+  "https://cinema-ticket-admin.netlify.app",
+  "https://cinema-ticket-fk.netlify.app",
   "http://localhost:1208",
   "http://localhost:3000",
 ];
